d79b560baa60cbe318e0a313280f0beb80fbf51c
ilyutoev <ilyutoev@gmail.com>
2022-02-12 22:54:09 +0300

    Test article rating

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import aiohttp
 import asyncio
 
+import pymorphy2
+
 from adapters.inosmi_ru import sanitize
+from text_tools import calculate_jaundice_rate
+from text_tools import split_by_words
 
 
 async def fetch(session, url):
@@ -13,8 +17,16 @@ async def fetch(session, url):
 async def main():
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
         html = await fetch(session, 'https://inosmi.ru/20220209/ukraina-252938463.html')
-        text = sanitize(html, plaintext=True)
-        print(text)
+    morph = pymorphy2.MorphAnalyzer()
 
+    text = sanitize(html, plaintext=True)
+
+    article_words = split_by_words(morph, text)
+
+    test_charged_words = ['удар', 'атака', 'агрессия']
+    rate = calculate_jaundice_rate(article_words, test_charged_words)
+
+    print('Рейтинг: ', rate)
+    print('Слов в статье: ', len(article_words))
 
 asyncio.run(main())
