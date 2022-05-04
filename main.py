7f744100364db4789a5f9f4c93b39a2d15b23610
ilyutoev <ilyutoev@gmail.com>
2022-05-04 23:13:55 +0300

    Add fetch exception processing

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import aiohttp
 import asyncio
 
 import pymorphy2
+from aiohttp import ClientError
 from anyio import create_task_group
 
+from adapters.constants import ProcessingStatus
 from adapters.inosmi_ru import get_title
 from adapters.inosmi_ru import sanitize
 from text_tools import calculate_jaundice_rate
@@ -18,17 +20,27 @@ async def fetch(session, url):
 
 
 async def process_article(session, morph, charged_words, url, result):
-    html = await fetch(session, url)
+
+    info = {
+        'title': None,
+        'rate': None,
+        'words_count': None,
+    }
+
+    try:
+        html = await fetch(session, url)
+        info['status'] = ProcessingStatus.OK
+    except ClientError:
+        info['status'] = ProcessingStatus.FETCH_ERROR
+        result.append(info)
+        return
 
     text = sanitize(html, plaintext=True)
     article_words = split_by_words(morph, text)
-    rate = calculate_jaundice_rate(article_words, charged_words)
-
-    result.append({
-        'title': get_title(html),
-        'rate': rate,
-        'words_count': len(article_words),
-    })
+    info['rate'] = calculate_jaundice_rate(article_words, charged_words)
+    info['title'] = get_title(html)
+    info['words_count'] = len(article_words),
+    result.append(info)
 
 
 async def main():
@@ -39,7 +51,7 @@ async def main():
         'https://inosmi.ru/20220504/ukraina-254051228.html',
         'https://inosmi.ru/20220504/drova-254050630.html',
         'https://inosmi.ru/20220504/bayden-254047336.html',
-        'https://inosmi.ru/20220504/ukraina-254051228.html',
+        'https://inosmi.ru/20220504/ukraina-25405122822.html',
     ]
     morph = pymorphy2.MorphAnalyzer()
 
@@ -54,6 +66,7 @@ async def main():
                 )
 
     for article_info in result:
+        print('Статус:', article_info['status'].value)
         print('Заголовок:', article_info['title'])
         print('Рейтинг:', article_info['rate'])
         print('Слов в статье:', article_info['words_count'])
